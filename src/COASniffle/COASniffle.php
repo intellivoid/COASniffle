4a89741a544c069f196ce65ae72de7b6a3b1e79f
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-14 15:54:10 -0300

    Added EmailAddress to \COASniffle\Objects\UserInformation

--- src/COASniffle/COASniffle.php
+++ src/COASniffle/COASniffle.php
@@ -23,7 +23,9 @@
 
     include_once($LocalDirectory . 'Handlers' . DIRECTORY_SEPARATOR . 'COA.php');
 
+    include_once($LocalDirectory . 'Objects' . DIRECTORY_SEPARATOR . 'UserInformation' . DIRECTORY_SEPARATOR . 'EmailAddress.php');
     include_once($LocalDirectory . 'Objects' . DIRECTORY_SEPARATOR . 'Permissions.php');
+    include_once($LocalDirectory . 'Objects' . DIRECTORY_SEPARATOR . 'UserInformation.php');
 
     include_once($LocalDirectory . 'Utilities' . DIRECTORY_SEPARATOR . 'ErrorResolver.php');
     include_once($LocalDirectory . 'Utilities' . DIRECTORY_SEPARATOR . 'RequestBuilder.php');
