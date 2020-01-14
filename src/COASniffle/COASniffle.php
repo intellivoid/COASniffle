7a8d4188d59ce6a7b6c371625895be93b8100f24
Netkas <45276434+Netkas@users.noreply.github.com>
2020-01-14 10:53:00 -0300

    Added ACM Configuration and AutoConfig

--- src/COASniffle/COASniffle.php
+++ src/COASniffle/COASniffle.php
@@ -3,6 +3,14 @@
 
     namespace COASniffle;
 
+    $LocalDirectory = __DIR__ . DIRECTORY_SEPARATOR;
+
+    include_once($LocalDirectory . 'AutoConfig.php');
+
+    if(class_exists('acm\acm') == false)
+    {
+        include_once(__DIR__ . DIRECTORY_SEPARATOR . 'acm' . DIRECTORY_SEPARATOR . 'acm.php');
+    }
 
     class COASniffle
     {
