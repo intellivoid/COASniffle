a6cd32be8eab0b1d5198dfceec61703d0fa39d7c
netkas <netkas@intellivoid.info>
2020-02-19 17:16:25 -0300

    Added method verifyCode() to \COASniffle\Handlers > OTL

--- src/COASniffle/Handlers/OTL.php
+++ src/COASniffle/Handlers/OTL.php
@@ -3,22 +3,44 @@
 
     namespace COASniffle\Handlers;
 
-
     use COASniffle\Exceptions\BadResponseException;
+    use COASniffle\Exceptions\OtlException;
+    use COASniffle\Exceptions\RequestFailedException;
+    use COASniffle\Exceptions\UnsupportedAuthMethodException;
+    use COASniffle\Objects\OtlUserResponse;
     use COASniffle\Utilities\RequestBuilder;
 
+    /**
+     * Class OTL
+     * @package COASniffle\Handlers
+     */
     class OTL
     {
-        public static function registerHost(string $remote_host, string $user_agent): string
+        /**
+         * Verifies the OTL code and returns the user once authenticated
+         *
+         * @param string $auth_code
+         * @param string $host_id
+         * @param string $user_agent
+         * @param string $vendor
+         * @return OtlUserResponse
+         * @throws BadResponseException
+         * @throws OtlException
+         * @throws RequestFailedException
+         * @throws UnsupportedAuthMethodException
+         */
+        public static function verifyCode(string $auth_code, string $host_id, string $user_agent, string $vendor): OtlUserResponse
         {
             $Parameters =  array(
-                'remote_host' => $remote_host,
+                'auth_code' => $auth_code,
+                'host_id' => $host_id,
                 'user_agent' => $user_agent,
+                'vendor' => $vendor
             );
             $Response = RequestBuilder::sendRequest(
-                'khm',
+                'otl',
                 array(
-                    'action' => "register_host",
+                    'action' => "verify_otl",
                 ), $Parameters
             );
 
@@ -30,9 +52,22 @@
 
             if($ResponseJson['status'] == false)
             {
-                throw new KhmException($Response['content'], $Parameters);
+                $StatusCode = 0;
+                $ErrorMessage = "Unknown";
+
+                if(isset($ResponseJson['status_code']))
+                {
+                    $StatusCode = (int)$ResponseJson['status_code'];
+                }
+
+                if(isset($ResponseJson['message']))
+                {
+                    $ErrorMessage = $ResponseJson['message'];
+                }
+
+                throw new OtlException($Response['content'], $Parameters, $StatusCode, $ErrorMessage);
             }
 
-            return $ResponseJson['host_id'];
+            return OtlUserResponse::fromArray($ResponseJson['account']);
         }
     }
